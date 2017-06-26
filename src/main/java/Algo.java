80e163cc45cd0e1af2f0f51d7563f9eb4f598008
chensheng <chensheng@xiaomi.com>
2017-06-26 18:14:42 +0800

    opt code

--- src/main/java/Algo.java
+++ src/main/java/Algo.java
@@ -3,8 +3,27 @@
  */
 public class Algo
 {
-    void print()
+    boolean searchWithMinComp(int[] a, int n, int x)
     {
-        System.out.println("hello");
+        if (a[n - 1] == x)
+        {
+            return true;
+        }
+        int backup = a[n - 1];
+        a[n - 1] = x;
+        for (int i = 0; ; i++)
+        {
+            if (a[i] == x)
+            {
+                a[n - 1] = backup;
+                if (i < n - 1)
+                {
+                    return true;
+                } else
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
