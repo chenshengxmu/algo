d58b42eb4d96571a5036996f90a58d2e8a6d6d48
chensheng <chensheng@redefine.global>
2018-07-16 21:57:12 +0800

    add files

--- src/main/java/Algo.java
+++ src/main/java/Algo.java
@@ -3,6 +3,40 @@
  */
 public class Algo
 {
+    void qs(int[] a, int low, int high)
+    {
+        if (low < high)
+        {
+        int p = pt(a, low, high);
+            qs(a, low, p-1);
+            qs(a, p+1, high);
+        }
+
+    }
+
+    int pt(int[] a, int low, int high)
+    {
+       int pv = a[high];
+       int i= low-1;
+        for (int j = low; j < high; j++) {
+            if (a[j] <= pv)
+            {
+                i++;
+                swap(a, i, j);
+            }
+        }
+        i++;
+        swap(a, i, high);
+       return i;
+    }
+
+    void swap(int[] a, int i, int j)
+    {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
     boolean searchWithMinComp(int[] a, int n, int x)
     {
         if (a[n - 1] == x)
@@ -26,5 +60,68 @@ public class Algo
                 }
             }
         }
+    }
+
+    void quickSort(int[] a, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = partition(a, low, high);
+            quickSort(a, low, pi);
+            quickSort(a, pi+1, high);
+        }
+    }
+
+    private int partition(int[] a, int low, int high) {
+        int pivot = a[low];
+        int i = low-1;
+        int j = high+1;
+        while (true)
+        {
+            do{
+                i++;
+            }while (a[i]< pivot);
+            do {
+                j--;
+            }while (a[j]>pivot);
+            if (i >= j)
+            {
+                return j;
+            }
+            int bak = a[j];
+            a[j] = a[i];
+            a[i] = bak;
+        }
+    }
+
+    int partition2(int arr[], int left, int right)
+    {
+        int i = left, j = right;
+        int tmp;
+        int pivot = arr[(left + right) / 2];
+
+        while (i <= j) {
+            while (arr[i] < pivot)
+                i++;
+            while (arr[j] > pivot)
+                j--;
+            if (i <= j) {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                i++;
+                j--;
+            }
+        };
+
+        return i;
+    }
+
+    void quickSort2(int arr[], int left, int right) {
+        int index = partition2(arr, left, right);
+        if (left < index - 1)
+            quickSort(arr, left, index - 1);
+        if (index < right)
+            quickSort(arr, index, right);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,17 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int a[] = {10, 7, 8, 9, 1, 5};
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
         Algo algo = new Algo();
-        algo.print();
+        algo.qs(a, 0, a.length-1);
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
     }
 }
